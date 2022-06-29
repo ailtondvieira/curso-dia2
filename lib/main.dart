8eb4bf3062ec791cf3fe15b9f6706d5b66b3b9c6
Ailton D. Vieira <f.futuretechcode@gmail.com>
2022-06-29 11:56:19 -0300

    aadwadwad awdw adwa dwa dwa dwa dw dwa dwa dwa d awd wa

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Olá Mundos',
+      title: 'Olá Mundosadwada',
       theme: ThemeData(
         // This is the theme of your application.
         //-
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
+        // Notice that the counter didn'st reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
