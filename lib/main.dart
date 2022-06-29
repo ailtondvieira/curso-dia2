8e7963b14aaee6d8f33f7adeed382c210e7a316e
Ailton D. Vieira <f.futuretechcode@gmail.com>
2022-06-29 13:02:35 -0300

    [change] changed text.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Olá Mundosadwadaadadawdas',
+      title: 'Olá Mundão.',
       theme: ThemeData(
         // This is the theme of your application.
         //-
